f50988e85f916d96d15c37097dfed87d3acc2c0e
atomr <wildkayote@yandex.ru>
2020-06-28 22:35:54 +0500

    add app with bugs in filter or map items(problem with item id)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import "./App.css";
+import Footer from "./components/Footer";
+import AddTodo from "./containers/AddTodo";
+import VisibleTodoList from "./containers/VisibleTodoList";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
