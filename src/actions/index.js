c98f2dc04d53918fb54ac27e561bbd834bda20d2
atomr@note <wildkayote@yandex.ru>
2020-06-28 17:46:52 +0500

    init

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./ActionTypes";
+
+{
+    type: ADD_TODO;
+    text: "Build my first Redux app";
+}
+
+{
+    type: TOGGLE_TODO;
+    index: 5;
+}
